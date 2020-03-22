94a1bc86878b3bfba0e83a7e2114ea24d950e7ec
free <oncwnuM3kr4gzkR8gi52AQSjKbTU@git.weixin.qq.com>
2020-03-22 22:09:06 +0800

    transaction manager

--- atcrowdfunding02-admin-webui/src/test/java/CrowdTest.java
+++ atcrowdfunding02-admin-webui/src/test/java/CrowdTest.java
@@ -3,9 +3,14 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import com.atguigu.crowd.entity.Admin;
+import com.atguigu.crowd.mapper.AdminMapper;
+import com.atguigu.crowd.service.api.AdminService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,52 +18,62 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 // 在类上标记必要的注解，Spring整合Junit
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml","classpath:spring-persist-tx.xml"})
 public class CrowdTest {
 	
 	@Autowired
 	private DataSource dataSource;
+
+	@Autowired
+	private AdminMapper adminMapper;
+
+
+	@Test
+	public void testInsertAdminMapper(){
+		Admin admin = new Admin(null, "jerry", "123123","杰瑞","jerry@qq.com",null);
+		int count = adminMapper.insert(admin);
+		System.out.println("插入 "+ count +"行。");
+	}
+
+
 	@Test
 	public void testConnection() throws SQLException {
 		Connection connection = dataSource.getConnection();
 		System.out.println(connection);
 	}
-//	@Autowired
-//	private AdminMapper adminMapper;
+
+	@Autowired
+	private AdminService adminService;
+
+	@Test
+	public void testTx() {
+		Admin admin = new Admin(null, "jerry322Exce", "123456", "杰瑞", "jerry@qq.com", null);
+		adminService.saveAdmin(admin);
+	}
 //
-//	@Autowired
-//	private AdminService adminService;
-//
-//	@Test
-//	public void testTx() {
-//		Admin admin = new Admin(null, "jerry", "123456", "杰瑞", "jerry@qq.com", null);
-//		adminService.saveAdmin(admin);
-//	}
-//
-//	@Test
-//	public void testLog() {
-//
-//		// 1.获取Logger对象，这里传入的Class对象就是当前打印日志的类
-//		Logger logger = LoggerFactory.getLogger(CrowdTest.class);
-//
-//		// 2.根据不同日志级别打印日志
-//		logger.debug("Hello I am Debug level!!!");
-//		logger.debug("Hello I am Debug level!!!");
-//		logger.debug("Hello I am Debug level!!!");
-//
-//		logger.info("Info level!!!");
-//		logger.info("Info level!!!");
-//		logger.info("Info level!!!");
-//
-//		logger.warn("Warn level!!!");
-//		logger.warn("Warn level!!!");
-//		logger.warn("Warn level!!!");
-//
-//		logger.error("Error level!!!");
-//		logger.error("Error level!!!");
-//		logger.error("Error level!!!");
-//	}
-//
+	@Test
+	public void testLog() {
+
+		// 1.获取Logger对象，这里传入的Class对象就是当前打印日志的类
+		Logger logger = LoggerFactory.getLogger(CrowdTest.class);
+		// 2.根据不同日志级别打印日志
+		logger.debug("Hello I am Debug level!!!");
+		logger.debug("Hello I am Debug level!!!");
+		logger.debug("Hello I am Debug level!!!");
+
+		logger.info("Info level!!!");
+		logger.info("Info level!!!");
+		logger.info("Info level!!!");
+
+		logger.warn("Warn level!!!");
+		logger.warn("Warn level!!!");
+		logger.warn("Warn level!!!");
+
+		logger.error("Error level!!!");
+		logger.error("Error level!!!");
+		logger.error("Error level!!!");
+	}
+
 //	@Test
 //	public void testInsertAdmin() {
 //		Admin admin = new Admin(null, "tom", "123123", "汤姆", "tom@qq.com", null);
