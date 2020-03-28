9a0f34d2a844a7094da0d218ebb0180281858f26
free <oncwnuM3kr4gzkR8gi52AQSjKbTU@git.weixin.qq.com>
2020-03-28 20:46:12 +0800

    postJson req

--- atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/TestHandler.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/TestHandler.java
@@ -2,11 +2,15 @@ package com.atguigu.crowd.mvc.handler;
 
 import com.atguigu.crowd.entity.Admin;
 import com.atguigu.crowd.service.api.AdminService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -22,5 +26,16 @@ public class TestHandler {
         modelMap.addAttribute("adminList", adminList);
 
         return "target";
+    }
+
+    @ResponseBody
+    @RequestMapping("/send/array/three.html")
+    public String testReceiverArrayThree(@RequestBody List<Integer> array){
+        Logger logger = LoggerFactory.getLogger(TestHandler.class);
+        for (Integer number : array){
+            logger.info("number :   -- >" + number);
+        }
+        return "success";
+
     }
 }
